acf78362979451ca054bdbae924da0f3f441b675
akshay <akshaybennygh@gmail.com>
2025-03-24 16:08:22 +0000

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<div className='bg-white text-black h-screen w-screen flex items-center justify-center'>
+			TripBee.io
+		</div>
+	)
+}
