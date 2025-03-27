e314c7e6bfbdf12fc27997080fbe42e1489a4d71
akshay <akshaybennygh@gmail.com>
2025-03-27 15:21:30 +0000

    more widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import AviasalesFlightScheduleWidget from '@/components/AviaSalesFlightScheduleW
 import AviasalesMapWidget from '@/components/AviaSalesMapWidget'
 import AviaSalesPopularWidget from '@/components/AviaSalesPopularWidget'
 import AviasalesSearchFormWidget from '@/components/AviaSalesSearchFormWidget'
-import Modal from '@/components/Modal'
 
 export default function Home() {
 	return (
 		<div className='bg-[#47B1E0] text-black min-h-screen max-w-screen flex flex-col space-y-[64px] items-center justify-center relative py-20'>
-			{/* <Modal /> */}
 			<AviasalesSearchFormWidget />
 			<AviasalesFlightScheduleWidget />
 			<AviasalesMapWidget />
