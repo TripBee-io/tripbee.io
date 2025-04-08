003cd5e6549850d8d41271f2beb11203f3e58c9a
akshay <akshaybennygh@gmail.com>
2025-04-08 15:55:56 +0100

    sentry

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,102 @@
+import {withSentryConfig} from '@sentry/nextjs';
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: '/ingest/static/:path*',
-				destination: 'https://eu-assets.i.posthog.com/static/:path*',
-			},
-			{
-				source: '/ingest/:path*',
-				destination: 'https://eu.i.posthog.com/:path*',
-			},
-			{
-				source: '/ingest/decide',
-				destination: 'https://eu.i.posthog.com/decide',
-			},
-		]
-	},
-	// This is required to support PostHog trailing slash API requests
-	skipTrailingSlashRedirect: true,
-	images: {
-		domains: ['www.akshaybenny.com'], // allow loading images from this domain
-	},
-	experimental: {
-		ppr: 'incremental',
-	},
-	devIndicators: {
-		appIsrStatus: true,
-		buildActivity: true,
-		buildActivityPosition: 'bottom-right',
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+								async rewrites() {
+																return [
+																								{
+																																source: '/ingest/static/:path*',
+																																destination: 'https://eu-assets.i.posthog.com/static/:path*',
+																								},
+																								{
+																																source: '/ingest/:path*',
+																																destination: 'https://eu.i.posthog.com/:path*',
+																								},
+																								{
+																																source: '/ingest/decide',
+																																destination: 'https://eu.i.posthog.com/decide',
+																								},
+																]
+								},
+								// This is required to support PostHog trailing slash API requests
+								skipTrailingSlashRedirect: true,
+								images: {
+																domains: ['www.akshaybenny.com'], // allow loading images from this domain
+								},
+								experimental: {
+																ppr: 'incremental',
+								},
+								devIndicators: {
+																appIsrStatus: true,
+																buildActivity: true,
+																buildActivityPosition: 'bottom-right',
+								},
+								typescript: {
+																ignoreBuildErrors: true,
+								},
+								eslint: {
+																ignoreDuringBuilds: true,
+								},
 }
 
-export default nextConfig
+export default withSentryConfig(withSentryConfig(nextConfig, {
+// For all available options, see:
+// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+org: "tripbee",
+project: "javascript-nextjs",
+
+// Only print logs for uploading source maps in CI
+silent: !process.env.CI,
+
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// This can increase your server load as well as your hosting bill.
+// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// side errors will fail.
+// tunnelRoute: "/monitoring",
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+
+// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// See the following for more information:
+// https://docs.sentry.io/product/crons/
+// https://vercel.com/docs/cron-jobs
+automaticVercelMonitors: true,
+}), {
+// For all available options, see:
+// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+org: "tripbee",
+project: "javascript-nextjs",
+
+// Only print logs for uploading source maps in CI
+silent: !process.env.CI,
+
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// This can increase your server load as well as your hosting bill.
+// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// side errors will fail.
+// tunnelRoute: "/monitoring",
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+
+// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// See the following for more information:
+// https://docs.sentry.io/product/crons/
+// https://vercel.com/docs/cron-jobs
+automaticVercelMonitors: true,
+});
