ae614042ef79e9eb11d7ea20acd2d11459ca4975
akshay <akshaybennygh@gmail.com>
2025-04-15 17:08:29 +0100

    robots edit

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,7 +1,7 @@
 export async function GET(_request: Request): Promise<Response> {
 	const robotsTxt = `User-agent: *
   Disallow: /private/
-  
+  Disallow: /studio/
   Sitemap: https://www.tripbee.io/sitemap.xml`
 	return new Response(robotsTxt, {
 		status: 200,
